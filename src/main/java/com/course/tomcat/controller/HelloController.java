d0683762f8883130b9ced3a1e0ba423035e668b3
samson aberi <16022293+samzaberi@users.noreply.github.com>
2024-02-13 11:03:25 +0300

    Update HelloController.java

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -13,4 +13,8 @@ public class HelloController {
         return "Now is " + LocalDateTime.now();
     }
 
+    @GetMapping(value = "/random")
+    int random(){
+        return ThreadLocalRandom.current().nextInt(0,1000);
+    }
 }
